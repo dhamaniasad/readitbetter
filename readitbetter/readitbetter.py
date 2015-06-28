25801e0b86891065a30ae5a8e898ef87f6ca598d
Asad Dhamani <dhamaniasad@gmail.com>
2015-06-29 04:11:16 +0530

    Add basic cleanup, test doc

--- readitbetter/readitbetter.py
+++ readitbetter/readitbetter.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+from lxml.html.clean import Cleaner
+
+
+def clean_up_html(html):
+    """
+    Cleans up the HTML and strips away unwanted attributes
+    :param html:
+    :return:
+    """
+    html_cleaner = Cleaner(scripts=True, javascript=True, comments=True, style=True,
+                           forms=True, annoying_tags=True, processing_instructions=True,
+                           links=True)
+    return html_cleaner.clean_html(html)
