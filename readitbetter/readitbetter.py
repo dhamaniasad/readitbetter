9c71d950abe643488847ac4522fb9fb136be8fac
Asad Dhamani <dhamaniasad@gmail.com>
2015-06-29 17:24:38 +0530

    Whitelisting

--- readitbetter/readitbetter.py
+++ readitbetter/readitbetter.py
@@ -20,8 +20,28 @@ def clean_up_html(html):
     bdo: Internationalization purposes
     blockquote: Not a nuisance.
     br: Useful formatting information
+    caption: Want to preserve tables
+    cite: Not a nuisance.
+    code: Want to preserve code snippets
+    col: Want to preserve tables
+    colgroup: Want to preserve tables
+    dd: Might be useful
+    dfn: same as abbr
+    div: Very widely used within content
+    dl: same as dd
+    dt: same as dd
+    em: same as b
+    embed: Want to retain embedded videos
+    h1-h6: Want to retain headings
+    header: same as h1-h6
+    hr: Same as br
+    i: Same as em
+    iframe: same as embed
     """
-    tags_whitelist = ['a', 'abbr', 'article', 'b', 'base', 'bdi', 'bdo', 'blockquote', 'br']
+    tags_whitelist = ['a', 'abbr', 'article', 'b', 'base', 'bdi', 'bdo', 'blockquote', 'br', 'caption',
+                      'cite', 'code', 'col', 'colgroup', 'dd', 'dfn', 'div', 'dl', 'dt', 'em',
+                      'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'hr', 'i']
+    embeds_whitelist = ['embed', 'iframe']
     # html_cleaner = Cleaner(scripts=True, javascript=True, comments=True, style=True,
     #                        forms=True, annoying_tags=True, processing_instructions=True,
     #                        links=True)
