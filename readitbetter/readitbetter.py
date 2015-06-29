b0b746c8ed53b70debb7d5354cdbe6c2261b2767
Asad Dhamani <dhamaniasad@gmail.com>
2015-06-29 16:14:36 +0530

    WIP on whitelist based cleanup

--- readitbetter/readitbetter.py
+++ readitbetter/readitbetter.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from lxml.html.clean import Cleaner
 from bs4 import BeautifulSoup
-
+import bleach
 
 def clean_up_html(html):
     """
@@ -9,43 +8,21 @@ def clean_up_html(html):
     :param html:
     :return:
     """
-    soup = BeautifulSoup(html, parser='html5lib')
-    remove_script_tags(soup)
-    remove_form_tags(soup)
-    remove_annoying_tags(soup)
+    #  Tags list from http://html5doctor.com/element-index/
+    """
+    Why a specific tag is allowed
+    a: We do not want to remove links from the document
+    abbr: It isn't a nuisance tag and might contain useful information
+    article: It will contain only information that's useful to us. If only everyone used it.
+    b: This stylistic element is usually contained within the article
+    base: Don't want broken relative urls
+    bdi: Internationalization purposes
+    bdo: Internationalization purposes
+    blockquote: Not a nuisance.
+    br: Useful formatting information
+    """
+    tags_whitelist = ['a', 'abbr', 'article', 'b', 'base', 'bdi', 'bdo', 'blockquote', 'br']
     # html_cleaner = Cleaner(scripts=True, javascript=True, comments=True, style=True,
     #                        forms=True, annoying_tags=True, processing_instructions=True,
     #                        links=True)
-    return soup
-
-
-def remove_script_tags(soup):
-    """
-    Removes all script tags from the source
-    :param soup:
-    :return:
-    """
-    for tag in soup.find_all('script'):
-        tag.extract()
-
-
-def remove_form_tags(soup):
-    """
-    Removes all forms from the source
-    :param soup:
-    :return:
-    """
-    for tag in soup.find_all('form'):
-        tag.extract()
-
-
-def remove_annoying_tags(soup):
-    """
-    Removes annoying tags like blink and marquee
-    :param soup:
-    :return:
-    """
-    for tag in soup.find_all('blink'):
-        tag.extract()
-    for tag in soup.find_all('marquee'):
-        tag.extract()
+    return
